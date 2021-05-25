f9977942fe7eb30009fbe43a2fc03f3daba3e262
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-05-25 15:48:14 +0300

    new

---
+++ newLaba6Vlad/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLaba6Vlad
+{
+    class Program
+    {
+        public static Vector3D Init(int x, int y, int z) => new Vector3D(x, y, z);
+
+        public static void Read()
+        {
+            Console.Write("Введите X: ");
+            int.TryParse(Console.ReadLine(), out int x);
+            Console.Write("Введите Y: ");
+            int.TryParse(Console.ReadLine(), out int y);
+            Console.Write("Введите Z: ");
+            int.TryParse(Console.ReadLine(), out int z);
+
+            Vector3D vector1 = Init(x, y, z);
+
+            Console.Write("Введите X: ");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.Write("Введите Y: ");
+            int.TryParse(Console.ReadLine(), out y);
+            Console.Write("Введите Z: ");
+            int.TryParse(Console.ReadLine(), out z);
+
+            Vector3D vector2 = Init(x, y, z);
+            Console.WriteLine("Vector 1: ");
+            Display(vector1);
+            Console.WriteLine("Vector 2: ");
+            Display(vector2);
+            vector1++;
+            vector2++;
+            Console.WriteLine((vector1 + vector2).ToString());
+            Console.WriteLine((vector1 - vector2).ToString());
+            Vector3D res = vector1.SumOfVectors(vector2);
+            Display(res);
+
+        }
+        public static void Display(Vector3D vector)
+        {
+            Console.WriteLine(vector);
+        }
+
+        static void Main(string[] args)
+        {
+            Read();
+        }
+    }
+}
---
+++ newLaba6Vlad/Vector3D.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newLaba6Vlad
+{
+    class Vector3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Vector3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public Vector3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3D(Vector3D vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+        }
+
+        public Vector3D SumOfVectors(Vector3D vector)
+        {
+            return new Vector3D(X + vector.X, Y + vector.Y, Z + vector.Z);
+        }
+        public Vector3D SubstractionOfVectors(Vector3D vector)
+        {
+            return new Vector3D(X - vector.X, Y - vector.Y, Z - vector.Z);
+        }
+
+        public int ScalarOfVectors(Vector3D vector)
+        {
+            return (X * vector.X) + (Y * vector.Y) + (Z * vector.Z);
+        }
+
+        public static Vector3D operator ++(Vector3D vector) => new Vector3D(vector.X + 1, vector.Y + 1, vector.Z + 1);
+        public static Vector3D operator --(Vector3D vector) => new Vector3D(vector.X - 1, vector.Y - 1, vector.Z - 1);
+
+        public static Vector3D operator +(Vector3D vector1, Vector3D vector2) => new Vector3D(vector1.X+vector2.X, vector1.Y + vector2.Y, vector1.Z + vector2.Z);
+        public static Vector3D operator -(Vector3D vector1, Vector3D vector2) => new Vector3D(vector1.X-vector2.X, vector1.Y - vector2.Y, vector1.Z - vector2.Z);
+
+        public static implicit operator string(Vector3D vector) => vector.ToString();
+        public override string ToString() => $"X: {X}. Y: {Y}. Z: {Z}.";
+    }
+}
